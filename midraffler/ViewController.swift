7218cc952a743dffff0a04aa91bb5c4e121a19ee
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-03-16 18:08:10 +0700

    feat: read from csv file and save result to a text file

--- midraffler/ViewController.swift
+++ midraffler/ViewController.swift
@@ -12,10 +12,12 @@ class ViewController: NSViewController {
 
     var isStarted = false
     var pathString: String?
+    var resultFile: String = "/Users/line/Documents/workspace/swift/first/midraffler/data/results.txt"
     let fm = FileManager.default
     var myTimer = Timer()
     var midResult: String = ""
-    
+    var nFiles: Int = 0
+    var resultArray = [String]()
 
     @IBOutlet weak var midTextField: NSTextField!
     
@@ -24,7 +26,7 @@ class ViewController: NSViewController {
         print("Start")
         midResult = getMidFromFiles()
         if isStarted == false {
-            runCountdown()
+            runAuto()
         }
     }
     
@@ -49,43 +51,92 @@ class ViewController: NSViewController {
         {
             pathString=""
         }
-    }
-    
-    func getMidFromFiles() -> String {
+        pathString = "/Users/line/Documents/workspace/swift/first/midraffler/data/"
         
-        do {
-            let nFiles:Int = try fm.contentsOfDirectory(atPath: pathString!).filter{ $0.hasPrefix("test") }.count
-            let randomFileNum:UInt32 = arc4random_uniform(UInt32(nFiles))
-            let randomFile:Int = Int(randomFileNum) + 1
-            
-            let filePath:String = pathString! + "/test-" + String(randomFile) + ".txt"
+        do{
+            // Count existing files
+            NSLog("Start counting files")
+            let nFiles:Int = try fm.contentsOfDirectory(atPath: pathString!).filter{ $0.hasPrefix("testing") }.count
+            NSLog("Counting files ended")
+            print("nFiles: " + String(nFiles))
             
             //Check file exist or not
-            if fm.fileExists(atPath: filePath) {
-                print("File exists")
-            } else {
-                print("File not found")
+            if fm.fileExists(atPath: resultFile) {
+                // Contain file's data in an array
+                let resultFileBuffer = fm.contents(atPath: resultFile)
+                let resultData = String(data: resultFileBuffer!, encoding: .utf8)
+                resultArray = (resultData?.components(separatedBy: .newlines))!
+                print(resultArray)
             }
-            
-            let databuffer = fm.contents(atPath: filePath)
-            let data = String(data: databuffer!, encoding: .utf8)
-            var myStrings = data?.components(separatedBy: "\n")
-            let randomIndexNum:UInt32 = arc4random_uniform(UInt32(myStrings!.count))
-            let randomIndex:Int = Int(randomIndexNum)
-            print("Data: " + (myStrings?[randomIndex])!)
-            
-            return (myStrings?[randomIndex])!
         } catch {
             print(error)
-            return " "
         }
     }
     
-    func runCountdown() {
+    // Pick random MID from Files
+    func getMidFromFiles() -> String {
+        
+        // Pick a random file
+        let randomFile:Int = randomNumber(n: nFiles) + 1
+        NSLog("Pick one random number for files")
+            
+        let filePath:String = pathString! + "testing-" + String(randomFile) + ".csv"
+        print(filePath)
+            
+        //Check file exist or not
+        if fm.fileExists(atPath: filePath) {
+            print("File exists")
+        } else {
+            print("File not found")
+            return " "
+        }
+        
+        // Count lines in the random file
+        NSLog("Start counting lines")
+        let databuffer = fm.contents(atPath: filePath)
+        let data = String(data: databuffer!, encoding: .utf8)
+        var myStrings = data?.components(separatedBy: .newlines)
+        NSLog("Counting lines ended")
+        print("nLines: " + String((myStrings!.count)))
+        
+        var randomIndex: Int = 0
+        
+        print(resultArray)
+        // Repeat pick random line until line does not exist in array
+        repeat {
+            // Pick a random line
+            randomIndex = randomNumber(n: (myStrings!.count))
+            NSLog("Pick one random number for lines")
+            print("Data: " + (myStrings?[randomIndex])!)
+        } while resultArray.contains((myStrings?[randomIndex])!)
+        
+        resultArray.append((myStrings?[randomIndex])!)
+        
+        // Parse line to be a result
+        let results = (myStrings?[randomIndex])!.components(separatedBy: ",")
+        var resultString: String = ""
+        for data in results {
+            resultString.append(data + " \n")
+        }
+        print("Result: " + resultString)
+        
+        return String(resultString)
+    }
+    
+    // Generate random number
+    func randomNumber(n: Int) -> Int {
+        let randomFileNum:UInt32 = arc4random_uniform(UInt32(n))
+        let randomNum:Int = Int(randomFileNum)
+        return randomNum
+    }
+    
+    // Make timer and run randomMid func every 0.125s
+    func runAuto() {
         isStarted = true
         myTimer = Timer.scheduledTimer(timeInterval: 0.125, target: self, selector:#selector(ViewController.randomMid), userInfo: nil, repeats: true)
     }
     
+    // Generate random MID and change UI Label
     func randomMid() {
         let seeds : String = "qwertyuiopasdfghjklzxcvbnm1234567890";
         let len = seeds.characters.count;
@@ -101,6 +152,18 @@ class ViewController: NSViewController {
         }
         
         midTextField.stringValue = "\(mid)"
+    }
+
+    override func viewWillDisappear() {
+        // Save results to file
+        let joinedResult = resultArray.joined(separator: "\n")
+        do{
+            try (joinedResult.write(toFile: resultFile, atomically: false, encoding: .utf8))
+        } catch {
+            print (error)
+        }
+        print(resultArray)
+        print("View will disappear")
     }
 }
 
