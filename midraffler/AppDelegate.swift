b92dea12dda7613f622bd4d7d81ab1f189bbc3d0
Matthew Tanudjaja <mrexmelle@gmail.com>
2017-03-16 23:28:32 +0700

    feat: Add directory selector

--- midraffler/AppDelegate.swift
+++ midraffler/AppDelegate.swift
@@ -9,18 +9,52 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+    @IBAction func onOpenClicked(_ sender: NSMenuItem)
+    {
+        print("onOpenClicked - Start")
+        
+        // get the ViewController instance
+        let vc=NSApplication.shared().mainWindow?.contentViewController as? ViewController
+        
+        let dialog=NSOpenPanel()
+        dialog.title="Choose a directory"
+        dialog.canChooseFiles=false
+        dialog.canChooseDirectories=true
+        dialog.canCreateDirectories=false
+        dialog.allowsMultipleSelection=false
+        
+        var csvDir: String?
+        if(dialog.runModal()==NSModalResponseOK)
+        {
+            let result=dialog.url
+            if(result != nil)
+            {
+                csvDir=result!.path
+                print("onOpenClicked - Directory chosen: " + csvDir!)
+            }
+            else
+            {
+                csvDir=""
+                print("onOpenClicked - Empty directory selected")
+            }
+            vc!.loadBaseDir(dir: csvDir!)
+        }
+        else
+        {
+            print("onOpenClicked - User canceled")
+        }
+    }
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification)
+    {
         // Insert code here to initialize your application
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
+    func applicationWillTerminate(_ aNotification: Notification)
+    {
         // Insert code here to tear down your application
     }
-
-
 }
 
--- midraffler/ViewController.swift
+++ midraffler/ViewController.swift
@@ -11,133 +11,150 @@ import Cocoa
 class ViewController: NSViewController {
 
     var isStarted = false
-    var pathString: String?
-    var resultFile: String = "/Users/line/Documents/workspace/swift/first/midraffler/data/results.txt"
-    let fm = FileManager.default
-    var myTimer = Timer()
-    var midResult: String = ""
-    var nFiles: Int = 0
-    var resultArray = [String]()
+    var baseDir: String?
+    var resFile: String?
+    let fileMgr = FileManager.default
+    var rndTimer = Timer()
+    var midResult: String?
+    var userDetail: String?
+    var fileCount: Int = 0
+    var resArray = [String]()
 
     @IBOutlet weak var midTextField: NSTextField!
     
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        let gesture = NSClickGestureRecognizer()
+        gesture.buttonMask=0x1
+        gesture.numberOfClicksRequired=1
+        gesture.target=self
+        gesture.action=#selector(ViewController.onMidClick)
+        midTextField.addGestureRecognizer(gesture)
+    }
+    
     @IBAction func onStartClick(_ sender: NSButton)
     {
-        print("Start")
+        print("onStartClick - start")
+        
+        if baseDir == nil
+        {
+            showMessage(message: "Please select [File] -> [Open] to load the base directory")
+            return
+        }
+        
         midResult = getMidFromFiles()
-        if isStarted == false {
+        print("onStartClick - midResult: \(midResult)")
+        if isStarted == false
+        {
             runAuto()
+        }
+    }
+    
+    func onMidClick(sender: NSGestureRecognizer)
+    {
+        if userDetail != nil
+        {
+            showMessage(message: userDetail!)
         }
     }
     
     @IBAction func onEndClick(_ sender: NSButton)
     {
-        print("Stop")
-        myTimer.invalidate()
-        isStarted = false
-        midTextField.stringValue = midResult
+        print("onEndClick - start")
         
+        if isStarted == false
+        {
+            print("onEndClick - isStarted: false")
+            showMessage(message: "Please start the raffle first!")
+            return
+        }
+        
+         print("onEndClick - isStarted: true - mid: \(midResult)")
+        
+        rndTimer.invalidate()
+        isStarted = false
+        midTextField.stringValue = midResult!
     }
     
-    override func viewDidLoad()
+    // pick random MID from Files
+    func getMidFromFiles() -> String
     {
-        super.viewDidLoad()
-
-        if CommandLine.argc > 1
+        print("getMidFromFiles - start")
+    
+        // pick a random file
+        let randomFile:Int = randomNumber(n: fileCount) + 1
+            
+        let filePath:String = baseDir! + "/testing-" + String(randomFile) + ".csv"
+        print(filePath)
+            
+        // check file exist or not
+        if fileMgr.fileExists(atPath: filePath)
         {
-            pathString=CommandLine.arguments[1];
+            print("getMidFromFiles - \(filePath) exists")
         }
         else
         {
-            pathString=""
-        }
-        pathString = "/Users/line/Documents/workspace/swift/first/midraffler/data/"
-        
-        do{
-            // Count existing files
-            NSLog("Start counting files")
-            let nFiles:Int = try fm.contentsOfDirectory(atPath: pathString!).filter{ $0.hasPrefix("testing") }.count
-            NSLog("Counting files ended")
-            print("nFiles: " + String(nFiles))
-            
-            //Check file exist or not
-            if fm.fileExists(atPath: resultFile) {
-                // Contain file's data in an array
-                let resultFileBuffer = fm.contents(atPath: resultFile)
-                let resultData = String(data: resultFileBuffer!, encoding: .utf8)
-                resultArray = (resultData?.components(separatedBy: .newlines))!
-                print(resultArray)
-            }
-        } catch {
-            print(error)
-        }
-    }
-    
-    // Pick random MID from Files
-    func getMidFromFiles() -> String {
-        
-        // Pick a random file
-        let randomFile:Int = randomNumber(n: nFiles) + 1
-        NSLog("Pick one random number for files")
-            
-        let filePath:String = pathString! + "testing-" + String(randomFile) + ".csv"
-        print(filePath)
-            
-        //Check file exist or not
-        if fm.fileExists(atPath: filePath) {
-            print("File exists")
-        } else {
-            print("File not found")
+            print("getMidFromFiles - \(filePath) does not exist")
             return " "
         }
         
-        // Count lines in the random file
-        NSLog("Start counting lines")
-        let databuffer = fm.contents(atPath: filePath)
+        // count lines in the random file
+        print("getMidFromFiles - start counting lines")
+        let databuffer = fileMgr.contents(atPath: filePath)
         let data = String(data: databuffer!, encoding: .utf8)
         var myStrings = data?.components(separatedBy: .newlines)
-        NSLog("Counting lines ended")
+        print("getMidFromFiles - counting lines ended")
         print("nLines: " + String((myStrings!.count)))
         
         var randomIndex: Int = 0
         
-        print(resultArray)
-        // Repeat pick random line until line does not exist in array
-        repeat {
+        print(resArray)
+        // repeat pick random line until line does not exist in array
+        repeat
+        {
             // Pick a random line
             randomIndex = randomNumber(n: (myStrings!.count))
-            NSLog("Pick one random number for lines")
+            print("Pick one random number for lines")
             print("Data: " + (myStrings?[randomIndex])!)
-        } while resultArray.contains((myStrings?[randomIndex])!)
+        }
+        while resArray.contains((myStrings?[randomIndex])!)
         
-        resultArray.append((myStrings?[randomIndex])!)
+        resArray.append((myStrings?[randomIndex])!)
+        print("getMidFromFiles - regsitered mid: \(resArray.last)")
         
-        // Parse line to be a result
+        // parse line to be a result
         let results = (myStrings?[randomIndex])!.components(separatedBy: ",")
-        var resultString: String = ""
-        for data in results {
-            resultString.append(data + " \n")
+        let resultString = results[0]
+        userDetail = ""
+        for data in results
+        {
+            userDetail = userDetail! + data + "\n"
         }
         print("Result: " + resultString)
         
         return String(resultString)
     }
     
-    // Generate random number
-    func randomNumber(n: Int) -> Int {
+    // generate random number
+    func randomNumber(n: Int) -> Int
+    {
         let randomFileNum:UInt32 = arc4random_uniform(UInt32(n))
         let randomNum:Int = Int(randomFileNum)
         return randomNum
     }
     
-    // Make timer and run randomMid func every 0.125s
-    func runAuto() {
+    // make timer and run randomMid func every 0.125s
+    func runAuto()
+    {
         isStarted = true
-        myTimer = Timer.scheduledTimer(timeInterval: 0.125, target: self, selector:#selector(ViewController.randomMid), userInfo: nil, repeats: true)
+        rndTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector:#selector(ViewController.randomiseMid), userInfo: nil, repeats: true)
     }
     
-    // Generate random MID and change UI Label
-    func randomMid() {
+    // generate random MID and change UI Label
+    func randomiseMid()
+    {
         let seeds : String = "qwertyuiopasdfghjklzxcvbnm1234567890";
         let len = seeds.characters.count;
         
@@ -153,17 +170,73 @@ class ViewController: NSViewController {
         
         midTextField.stringValue = "\(mid)"
     }
+    
+    // reinitialise base directory
+    func loadBaseDir(dir: String)
+    {
+        baseDir=dir
+        resFile=baseDir!+"/results.txt"
+        
+        print("loadBaseDir - baseDir: \(baseDir)")
+        print("loadBaseDir - resFile: \(resFile)")
+        
+        do
+        {
+            // count existing files
+            NSLog("Start counting files")
+            fileCount = try fileMgr.contentsOfDirectory(atPath: baseDir!).filter{ $0.hasPrefix("testing") }.count
+            NSLog("Counting files ended")
+            print("fileCount: \(fileCount)")
+            
+            // check file exist or not
+            if fileMgr.fileExists(atPath: resFile!)
+            {
+                // contain file's data in an array
+                let resFileBuffer = fileMgr.contents(atPath: resFile!)
+                let resultData = String(data: resFileBuffer!, encoding: .utf8)
+                resArray = (resultData?.components(separatedBy: .newlines))!
+                print(resArray)
+            }
+            else
+            {
+                fileMgr.createFile(atPath: resFile!, contents: Data())
+            }
+        }
+        catch
+        {
+            print("loadBaseDir - error: \(error)")
+        }
+        
+        midTextField.stringValue = "Hello"
+        midResult=""
+    }
 
-    override func viewWillDisappear() {
-        // Save results to file
-        let joinedResult = resultArray.joined(separator: "\n")
-        do{
-            try (joinedResult.write(toFile: resultFile, atomically: false, encoding: .utf8))
-        } catch {
+    override func viewWillDisappear()
+    {
+        // save results to file
+        let joinedResult = resArray.joined(separator: "\n")
+        do
+        {
+            print("viewWillDisappear - resFile: \(resFile)")
+            // nil means, no baseDir ever defined
+            if resFile != nil
+            {
+                try(joinedResult.write(toFile: resFile!, atomically: false, encoding: .utf8))
+            }
+        }
+        catch
+        {
             print (error)
         }
-        print(resultArray)
-        print("View will disappear")
+        print(resArray)
+        print("viewWillDisappear - exit")
+    }
+    
+    func showMessage(message: String)
+    {
+        let alert=NSAlert()
+        alert.messageText=message
+        alert.runModal()
     }
 }
 
